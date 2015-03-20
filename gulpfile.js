d753379bc6bd155f143fe672985c89ddc0eb9278
Daniel <frontend@reddesignsystems.com>
2015-03-20 13:53:04 -0500

    DataTable ready to complie with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,11 @@ var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
     mix.less("style.less", "assets/css");
-    mix.scripts([
 
-    ], "resources/assets/js", "assets/js");
+    mix.scripts([
+    	"libs/bootstrap/bootstrap.js",
+    	"libs/datatable/jquery.dataTables.js",
+    	"libs/jquery/jquery.js"
+    ], "assets/js", "resources/assets/js");
+
 });
