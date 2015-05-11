fe9c60fc13ab4fed7ee0b3ce48e4a9ef762fca7c
Daniel H <frontend@reddesignsystems.com>
2015-05-11 11:17:18 -0500

    Change initialize dataTables and style css for show/hide columns dataTables

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -134,110 +134,96 @@
 	DATA - TABLES 
 
 */
+ 	$('table.pipoca').dataTable({
+ 			
+ 	});
 	var table = $('#dataTable').dataTable({
-    "dom": 'C<"clear">lfrtip',
 		language: {
-		    "emptyTable":     "Datos en la tabla no validos",
-		    "info":           "_START_ / _END_ of _TOTAL_ entries",
-		    "infoEmpty":      "Showing 0 to 0 of 0 entries",
-		    "infoFiltered":   "(filtered from _MAX_ total entries)",
-		    "infoPostFix":    "",
-		    "thousands":      ",",
-		    "lengthMenu":     "Show _MENU_ entries",
-		    "loadingRecords": "Cargando...",
-		    "processing":     "Procesando...",
-		    "search":         "Busqueda:",
-		    "zeroRecords":    "No matching records found",
-		    "paginate": {
-		        "first":      "First",
-		        "last":       "Last",
-		        "next":       "Siguiente",
-		        "previous":   "Anterior"
-	    	},
-		    "aria": {
-		        "sortAscending":  ": activate to sort column ascending",
-		        "sortDescending": ": activate to sort column descending"
-		    }
+	    "emptyTable":     "Datos en la tabla no validos",
+	    "infoPostFix":    "",
+	    "thousands":      ",",
+	    "loadingRecords": "Cargando...",
+	    "processing":     "Procesando...",
+	    "search":         "Busqueda:",
+	    "zeroRecords":    "No matching records found",
+	    "paginate": {
+        "first":        "First",
+        "last":         "Last",
+        "next":         "Siguientes",
+        "previous":     "Anterior"
+    	}
 		},
+		"ordering": false,
+    "info":     false,
+    "dom": 'C<"clear">lfrtip',		
 		"scrollY": "300px"
 	});
 
-	var table2 = $('#dataTable2').dataTable({
+	var table2 = $('#dataTable2').dataTable({		
 		language: {
-		    "emptyTable":     "Datos en la tabla no validos",
-		    "info":           "_START_ / _END_ of _TOTAL_ entries",
-		    "infoEmpty":      "Showing 0 to 0 of 0 entries",
-		    "infoFiltered":   "(filtered from _MAX_ total entries)",
-		    "infoPostFix":    "",
-		    "thousands":      ",",
-		    "lengthMenu":     "Show _MENU_ entries",
-		    "loadingRecords": "Cargando...",
-		    "processing":     "Procesando...",
-		    "search":         "Busqueda:",
-		    "zeroRecords":    "No matching records found",
-		    "paginate": {
-		        "first":      "First",
-		        "last":       "Last",
-		        "next":       "Siguiente",
-		        "previous":   "Anterior"
-	    	},
-		    "aria": {
-		        "sortAscending":  ": activate to sort column ascending",
-		        "sortDescending": ": activate to sort column descending"
-		    }
-		},
-		"scrollY": "300px",
-        "scrollCollapse": true,
-        "paging":         false,
-        "sDom": 'T<"clear">lfrtip',
-        "oTableTools": {
-	        "sSwfPath": "assets/swf/copy_csv_xls_pdf.swf",
-	        "aButtons": [
-	      		{
-		            "sExtends":	"copy",
-		          	"sButtonText": "Copiar"
-	      		},
-	      		{
-		            "sExtends":	"print",
-		          	"sInfo" : "Por favor, pulse ESC para devolverse",
-		          	"sButtonText": "Imprimir"
-	      		},
-		        {
-		            "sExtends":    "collection",
-		            "sButtonText": "Guardar",
-		            "aButtons": [ "csv", "xls", "pdf" ]
-		        }
-        	]
+	    "emptyTable":     "Datos en la tabla no validos",
+	    "infoPostFix":    "",
+	    "thousands":      ",",
+	    "loadingRecords": "Cargando...",
+	    "processing":     "Procesando...",
+	    "search":         "Busqueda:",
+	    "zeroRecords":    "No matching records found",
+	    "paginate": {
+        "first":        "First",
+        "last":         "Last",
+        "next":         "Siguientes",
+        "previous":     "Anterior"
     	}
+		},
+		"ordering": false,
+    "info":     false,
+		"scrollY": "300px",
+    "scrollCollapse": true,
+    "paging":         false,
+    "sDom": 'T<"clear">lfrtip',
+    "oTableTools": {
+      "sSwfPath": "assets/swf/copy_csv_xls_pdf.swf",
+      "aButtons": [
+    		{
+            "sExtends":	"copy",
+          	"sButtonText": "Copiar"
+    		},
+    		{
+            "sExtends":	"print",
+          	"sInfo" : "Por favor, pulse ESC para devolverse",
+          	"sButtonText": "Imprimir"
+    		},
+        {
+            "sExtends":    "collection",
+            "sButtonText": "Guardar",
+            "aButtons": [ "csv", "xls", "pdf" ]
+        }
+    	]
+    }
 	});
 
-	var table3 = $('#dataTable3').DataTable( {
+	var table3 = $('#dataTable3').DataTable( {		
 		language: {
-		    "emptyTable":     "Datos en la tabla no validos",
-		    "info":           "_START_ / _END_ of _TOTAL_ entries",
-		    "infoEmpty":      "Showing 0 to 0 of 0 entries",
-		    "infoFiltered":   "(filtered from _MAX_ total entries)",
-		    "infoPostFix":    "",
-		    "thousands":      ",",
-		    "lengthMenu":     "Show _MENU_ entries",
-		    "loadingRecords": "Cargando...",
-		    "processing":     "Procesando...",
-		    "search":         "Busqueda:",
-		    "zeroRecords":    "No matching records found",
-		    "paginate": {
-		        "first":      "First",
-		        "last":       "Last",
-		        "next":       "Siguiente",
-		        "previous":   "Anterior"
-		    },
-		    "aria": {
-		        "sortAscending":  ": activate to sort column ascending",
-		        "sortDescending": ": activate to sort column descending"
-		    }
+	    "emptyTable":     "Datos en la tabla no validos",
+	    "infoPostFix":    "",
+	    "thousands":      ",",
+	    "loadingRecords": "Cargando...",
+	    "processing":     "Procesando...",
+	    "search":         "Busqueda:",
+	    "zeroRecords":    "No matching records found",
+	    "paginate": {
+        "first":        "First",
+        "last":         "Last",
+        "next":         "Siguientes",
+        "previous":     "Anterior"
+    	}
 		},
-  		dom: 'C<"clear">lfrtip',
+		"ordering": false,
+    "info":     false,
+  	dom: 'C<"clear">lfrtip',
 		colVis: {
-			exclude: [],
+			"buttonText": "Ocultar Años",
+			exclude: ['all'],
 			groups: [
 				{
 					title: "2013-2017",
@@ -273,75 +259,73 @@
 	});
 	new $.fn.dataTable.FixedColumns( table3 );
 
-	var table4 = $('#dataTable4').DataTable( {
-		language: {
-		    "emptyTable":     "Datos en la tabla no validos",
-		    "info":           "_START_ / _END_ of _TOTAL_ entries",
-		    "infoEmpty":      "Showing 0 to 0 of 0 entries",
-		    "infoFiltered":   "(filtered from _MAX_ total entries)",
-		    "infoPostFix":    "",
-		    "thousands":      ",",
-		    "lengthMenu":     "Show _MENU_ entries",
-		    "loadingRecords": "Cargando...",
-		    "processing":     "Procesando...",
-		    "search":         "Busqueda:",
-		    "zeroRecords":    "No matching records found",
-		    "paginate": {
-		        "first":      "First",
-		        "last":       "Last",
-		        "next":       "Siguiente",
-		        "previous":   "Anterior"
-		    },
-		    "aria": {
-		        "sortAscending":  ": activate to sort column ascending",
-		        "sortDescending": ": activate to sort column descending"
-		    }
+	var table4 = $('#dataTable4').DataTable( {	
+	  language: {
+	    "emptyTable":     "Datos en la tabla no validos",
+	    "infoPostFix":    "",
+	    "thousands":      ",",
+	    "loadingRecords": "Cargando...",
+	    "processing":     "Procesando...",
+	    "search":         "Busqueda:",
+	    "zeroRecords":    "No matching records found",
+	    "paginate": {
+        "first":        "First",
+        "last":         "Last",
+        "next":         "Siguientes",
+        "previous":     "Anterior"
+    	}
 		},
+		"ordering": false,
+    "info":     false,	
 		"dom": "Cfrtip",
 		"scrollY": "400px",
 		"scrollX": true,
 		"scrollCollapse": true,
 		"paging": false,
 		"columns": [
-		    { "width": "25%" },
-		    null,
-		    null,
-		    null,
-		    null,
-		    null,
-		    null,
-		    null,
-		    null,
-		    null,
-		    null,
-		    null,
-		    null,
-		    null,
-		    null
+	    { "width": "25%" },
+	    null,
+	    null,
+	    null,
+	    null,
+	    null,
+	    null,
+	    null,
+	    null,
+	    null,
+	    null,
+	    null,
+	    null,
+	    null,
+	    null
 		]
 	});
 	new $.fn.dataTable.FixedColumns( table4, {
-	    leftColumns: 1,
-	    rightColumns: 1
-    });
+    leftColumns: 1,
+    rightColumns: 1
+  });
 
-  	var table5 = $('#dataTable5').dataTable({
-  		language: {
-	  	    "emptyTable":     "Datos en la tabla no validos",
-	  	    "info":           "_START_ / _END_ of _TOTAL_ entries",
-	  	    "infoEmpty":      "Showing 0 to 0 of 0 entries",
-	  	    "infoFiltered":   "(filtered from _MAX_ total entries)",
-	  	    "infoPostFix":    "",
-	  	    "thousands":      ",",
-	  	    "lengthMenu":     "Show _MENU_ entries",
-	  	    "loadingRecords": "Cargando...",
-	  	    "processing":     "Procesando...",
-	  	    "search":         "Busqueda:",
-	  	    "zeroRecords":    "No matching records found"  	    
-	  	},
-	  	"paging": false,
-	  	"ordering": false
-  	});
+	var table5 = $('#dataTable5').dataTable({ 
+		"sDom": '<"top"i>rt<"bottom"flp><"clear">',
+	  language: {
+	    "emptyTable":     "Datos en la tabla no validos",
+	    "infoPostFix":    "",
+	    "thousands":      ",",
+	    "loadingRecords": "Cargando...",
+	    "processing":     "Procesando...",
+	    "search":         "Busqueda:",
+	    "zeroRecords":    "No matching records found",
+	    "paginate": {
+        "first":        "First",
+        "last":         "Last",
+        "next":         "Siguientes",
+        "previous":     "Anterior"
+    	}
+		},
+		"ordering": false,
+    "info":     false, 		
+  	"paging": false,
+	});
 	
 	
 /*
