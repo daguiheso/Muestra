324c99c949289fa787f6f4a2102c4002ff1d4b70
Guillermo Hernandez <frontend@reddesignsystems.com>
2015-05-22 14:17:07 +0200

    Add graphic pie

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,8 @@ elixir(function(mix) {
         "libs/d3/d3.js",
         "libs/d3/radial/RadarCharts.js",
         "libs/d3/radial/script.js",
+        "libs/d3/pie/donut3d.js",
+        "libs/d3/pie/script.js",
         "pipoka/main.js",
         "pipoka/tables/tables.js",
         "pipoka/datatable/datatable.js",
